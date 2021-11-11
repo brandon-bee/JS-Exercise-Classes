d40eeeb50cb450953dbd480d05ee2007a0752991
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2021-11-11 12:47:44 -0800

    Finished all tasks, passed all tests

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ class Car {
       return `I ran out of fuel at ${this.odometer} miles!`
     };
   };
-}
+};
 
 /*
   TASK 3
@@ -117,7 +117,7 @@ class Lambdasian {
   speak() {
     return `Hello my name is ${this.name}. I am from ${this.location}`;
   };
-}
+};
 
 /*
   TASK 4
@@ -133,9 +133,21 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian {
+  constructor(attrs) {
+    super(attrs);
+    this.specialty = attrs.specialty;
+    this.favLanguage = attrs.favLanguage;
+    this.catchPhrase = attrs.catchPhrase;
+  };
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  };
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}`;
+  };
+};
 
-}
 /*
   TASK 5
     - Write a Student class extending Lambdasian.
@@ -151,9 +163,23 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-   
-}
+class Student extends Lambdasian {
+  constructor(attrs) {
+    super(attrs);
+    this.previousBackground = attrs.previousBackground;
+    this.className = attrs.className;
+    this.favSubjects = attrs.favSubjects;
+  };
+  listSubjects() {
+    return `Loving ${this.favSubjects}!`;
+  };
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`;
+  };
+  sprintChallenge(subject) {
+    return `${this.name} has begun sprint challenge on ${subject}`;
+  };
+};
 
 /*
   TASK 6
@@ -168,9 +194,20 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-   
-}
+class ProjectManager extends Instructor {
+   constructor(attrs) {
+    super(attrs);
+    this.gradClassName = attrs.gradClassName;
+    this.favInstructor = attrs.favInstructor;
+   };
+   standUp(channel) {
+    return `${this.name} announces to ${channel}, @channel standy times!`;
+   };
+   debugsCode(student, subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}`;
+   };
+};
+
 /*
   STRETCH PROBLEM (no tests!)
     - Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
